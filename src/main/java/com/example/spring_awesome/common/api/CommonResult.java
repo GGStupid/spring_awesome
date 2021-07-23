ad5288572fd92be3aa32e0f1c2fb21f1307da298
GGStupid <610383057@qq.com>
2021-07-23 17:45:56 +0800

    feat:brand

--- src/main/java/com/example/spring_awesome/common/api/CommonResult.java
+++ src/main/java/com/example/spring_awesome/common/api/CommonResult.java
@@ -1,122 +1,138 @@
 package com.example.spring_awesome.common.api;
 
 public class CommonResult<T> {
-    private long code;
-    private String message;
-    private T data;
+	private long code;
+	private String message;
+	private T data;
 
-    public CommonResult(long code, String message, T data) {
-        this.code = code;
-        this.message = message;
-        this.data = data;
-    }
+	public CommonResult(long code, String message, T data) {
+		this.code = code;
+		this.message = message;
+		this.data = data;
+	}
 
-    public CommonResult() {
-    }
+	public CommonResult() {
+	}
 
-    /**
-     * success
-     *
-     * @param data
-     * @param <T>
-     * @return
-     */
-    public static <T> CommonResult<T> success(T data) {
-        return new CommonResult<T>(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMessage(), data);
-    }
+	/**
+	 * 成功返回结果
+	 *
+	 * @param data
+	 * @param <T>
+	 * @return
+	 */
+	public static <T> CommonResult<T> success(T data) {
+		return new CommonResult<T>(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMessage(), data);
+	}
 
-    /** success
-     * @param data
-     * @param message
-     * @param <T>
-     * @return
-     */
-    public static <T> CommonResult<T> success(T data, String message) {
-        return new CommonResult<T>(ResultCode.SUCCESS.getCode(), message, data);
-    }
+	/**
+	 * 成功返回结果
+	 *
+	 * @param data    获取的数据
+	 * @param message 提示信息
+	 * @param <T>
+	 * @return
+	 */
+	public static <T> CommonResult<T> success(T data, String message) {
+		return new CommonResult<T>(ResultCode.SUCCESS.getCode(), message, data);
+	}
 
-    /** failed
-     * @param errorCode
-     * @param <T>
-     * @return
-     */
-    public static <T> CommonResult<T> failed(IErrorCode errorCode) {
-        return new CommonResult<>(errorCode.getCode(), errorCode.getMessage(), null);
-    }
+	/**
+	 * 失败返回结果
+	 *
+	 * @param errorCode 错误码
+	 * @param <T>
+	 * @return
+	 */
+	public static <T> CommonResult<T> failed(IErrorCode errorCode) {
+		return new CommonResult<>(errorCode.getCode(), errorCode.getMessage(), null);
+	}
 
-    /** failed
-     * @param message
-     * @param <T>
-     * @return
-     */
-    public static <T> CommonResult<T> failed(String message) {
-        return new CommonResult<>(ResultCode.FAILED.getCode(), message, null);
-    }
+	/**
+	 * 失败返回结果
+	 *
+	 * @param message 提示信息
+	 * @param <T>
+	 * @return
+	 */
+	public static <T> CommonResult<T> failed(String message) {
+		return new CommonResult<>(ResultCode.FAILED.getCode(), message, null);
+	}
 
-    /** failed
-     * @param <T>
-     * @return
-     */
-    public static <T> CommonResult<T> failed() {
-        return failed(ResultCode.FAILED);
-    }
+	/**
+	 * 失败返回结果
+	 *
+	 * @param <T>
+	 * @return
+	 */
+	public static <T> CommonResult<T> failed() {
+		return failed(ResultCode.FAILED);
+	}
 
-    /** validateFailed
-     * @param <T>
-     * @return
-     */
-    public static <T> CommonResult<T> validateFailed() {
-        return failed(ResultCode.VALIDATE_FAILED);
-    }
+	/**
+	 * 参数验证失败返回结果
+	 *
+	 * @param <T>
+	 * @return
+	 */
+	public static <T> CommonResult<T> validateFailed() {
+		return failed(ResultCode.VALIDATE_FAILED);
+	}
 
-    /** validateFailed
-     * @param message
-     * @param <T>
-     * @return
-     */
-    public static <T> CommonResult<T> validateFailed(String message) {
-        return new CommonResult<>(ResultCode.VALIDATE_FAILED.getCode(), message, null);
-    }
+	/**
+	 * 参数验证失败返回结果
+	 *
+	 * @param message
+	 * @param <T>
+	 * @return
+	 */
+	public static <T> CommonResult<T> validateFailed(String message) {
+		return new CommonResult<>(ResultCode.VALIDATE_FAILED.getCode(), message, null);
+	}
 
-    /** unauthorized
-     * @param data
-     * @param <T>
-     * @return
-     */
-    public static <T> CommonResult<T> unauthorized(T data) {
-        return new CommonResult<>(ResultCode.UNAUTHORIZED.getCode(), ResultCode.UNAUTHORIZED.getMessage(), data);
-    }
+	/**
+	 * 未登录返回结果
+	 *
+	 * @param data
+	 * @param <T>
+	 * @return
+	 */
+	public static <T> CommonResult<T> unauthorized(T data) {
+		return new CommonResult<>(ResultCode.UNAUTHORIZED.getCode(), ResultCode.UNAUTHORIZED.getMessage(), data);
+	}
 
-    /** forbidden
-     * @param data
-     * @param <T>
-     * @return
-     */
-    public static <T> CommonResult<T> forbidden(T data) {
-        return new CommonResult<>(ResultCode.FORBIDDEN.getCode(), ResultCode.FORBIDDEN.getMessage(), data);
-    }
+	/**
+	 * 未授权返回结果
+	 *
+	 * @param data
+	 * @param <T>
+	 * @return
+	 */
+	public static <T> CommonResult<T> forbidden(T data) {
+		return new CommonResult<>(ResultCode.FORBIDDEN.getCode(), ResultCode.FORBIDDEN.getMessage(), data);
+	}
 
-    public long getCode() {
-        return code;
-    }
+	public long getCode() {
+		return code;
+	}
 
-    public void setCode(long code) {
-        this.code = code;
-    }
+	public void setCode(long code) {
+		this.code = code;
+	}
 
-    public String getMessage() {
-        return message;
-    }
+	public String getMessage() {
+		return message;
+	}
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
+	public void setMessage(String message) {
+		this.message = message;
+	}
 
-    public T getData() {
-        return data;
-    }
+	public T getData() {
+		return data;
+	}
 
-    public void setData(T data) {
-        this.data = data;
-    }
+	public void setData(T data) {
+		this.data = data;
+	}
 }
